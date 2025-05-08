b9b702fc0bca6ba31dc16ce31381f0e1f4ba5a82
Your Name <you@example.com>
2025-05-08 18:40:22 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,287 @@
+// task1
+// let a= +prompt("eded daxil et")
+// if (a>0){
+//     console.log("eded musbetdir")
+// }else if (a==0){
+//     console.log ("eded o-a beraberdir")
+// }else if(a<0){
+//     console.log("eded menfidir")
+// }
+// task2
+// let b= +prompt("eded daxil et")
+// if ( b%2 ==0 ){
+//     console.log("eded cutdur")
+// } else {
+//     console.log("eded tekdir")
+// }
+// task3
+// let c= +prompt("birinci eded daxil et")
+// let d= +prompt("ikinci eded daxil et")
+// if(c==d){
+//     console.log("ededler beraberdir")
+// }else{
+//     console.log("ededler ferqlidir")
+// }
+// task4
+// let e= +prompt("write number")
+// if(e>0){
+//     console.log("eded 0-dan boyukdur")
+// }else if(e==0){
+//     console.log("eded o-a beareberdir")
+// }else{
+//     console.log("eded 0-dan kicikdir")
+// }
+// task-5
+// let f= +prompt("Yasinizi yazin")
+// if(f<18){
+//     console.log("yasi 18-den kicikdir")
+// }
+// else if (f==18){
+//     	console.log("yasi 18-e beraberdir")
+// }else{
+//     console.log("yasi 18-den boyukdur")
+// }
+// task-6
+// let bal=+prompt("balinizi yazin")
+// if(200<=bal && bal<=700){
+//     console.log("siz uniye qebul oldunuz")
+// }else if(bal<200){
+//     console.log("uniye qebul olmadiniz")
+// }else if(bal>700){
+//     console.log("yalnis")
+// }
+// task-7
+// let temp= +prompt("havanin derecesini yaz")
+// if(temp>0){
+//     console.log("Havada istilik-" + temp)
+// }else if(temp<=0){
+//     console.log("Havda saxta-" + temp)
+// }
+// task-8
+// let reqem=+prompt("reqem yaz")
+// if(5<=reqem && reqem<=10){
+//     console.log("reqem 5ile  10 araligindadir")
+// }else{
+//     console.log("reqem 5-10 araliginda deyil")
+// }
+// task-9
+// let gun=+prompt("Gunu daxil et")
+// if(gun==6){
+//     console.log("heftenin senbe gunudur")
+// }else if(gun==7){
+//     console.log("heftenin bazar gunudur")
+// }else{
+//     console.log("senbe veya bazar deyil")
+// }
+// task-10
+// let parol="abc123"
+// let sifre= prompt("sifrenizi daxil edin")
+// if(parol==sifre){
+//     console.log("parol duzgundur")
+// }else{
+//     console.log("parol sehvdir")
+// }
+// task-11
+// let r= +prompt("eded daxil et")
+// if(r%3==0){
+//     console.log(r/3)
+// }else{
+//     console.log("eded 3-e tam bolunmur")
+// }
+// task-12
+// let l = +prompt("reqem daxil et")
+// if (l%2==0 && l%4==0){
+//     console.log("eded hem 2-e hem 4-e bolunur")
+// }else{
+//     console.log("eded hem 2-e hem 4-e bolunmur")
+// }
+// // Input ilə daxil edilən bir ədədin 10-dan böyük və 20-dən kiçik olub olmadığını yoxlayan alqoritm yazın.
+// let a= +prompt("reqem daxil et")
+// if (a>10 && a<20){
+//     console.log("eded 10dan boyuk 20den kicikdir")
+// }else if(a<10){
+//     console.log("eded 10dan balacadir")
+// }else if (a>20){
+//     console.log("eded 20den boyukdur")
+// }
+// Input ilə daxil edilən iki ədədin bir-birinə bərabər olub olmadığını yoxlayan alqoritm yazın.
+// let a= +prompt("ilk reqemi yaz")
+// let b= +prompt("ikinci reqemi yaz")
+// if (a==b){
+//     console.log("a=b")
+// }else if (a > b) {
+//     console.log("a > b");
+// } else {
+//     console.log("a < b")
+// }
+// Input ilə daxil edilən bir string dəyərin "Salam" sözünə bərabər olub olmadığını yoxlayan alqoritm yazın.
+// let soz= prompt("bir soz daxil edin")
+// if (soz==="Salam"){
+//     console.log("daxil etdiyiniz soz 'Salam'dir ")
+// }else{
+//     console.log("daxil etdiyiniz soz 'Salam' deyildir")
+// }
+// Input ilə daxil edilən bir ədədin mənfi olub olmadığını yoxlayan və mənfidirsə, onun müsbət qarşılığını (modulunu) ekrana çıxaran alqoritm yazın.
+// let b=+prompt("reqemi yaz")
+// if (b<0){
+//     console.log("eded menfidir, musbet qarsiligi " + ( -b))
+// }else if(b==0){
+//     console.log("eded 0-a beraberdir")
+// } else if(b>0){
+//     console.log("eded musbetdir ve menfi qarsiligi " + ( -b))
+// }
+// Input ilə daxil edilən üç fərqli ədəddən ən kiçiyini tapan alqoritm yazın.
+// let a=+prompt("ilk ededi yaz")
+// let b=+prompt("ikinci ededi yaz")
+// let c=+prompt("ucuncu ededi yaz")
+// if (a<b && a<c){
+//     console.log(a + " en kicikdir")
+// }else if (b<a && c>b){
+//     console.log(b + " en kicikdir")
+// }else if (c<b && c<a){
+//     console.log(c + " en kicikdir")
+// }
+// Input ilə daxil edilən bir ədədin 7-yə bölündükdə qalığının 0 olub olmadığını yoxlayan alqoritm yazın.
+// let d= +prompt("reqem yaz")
+// if(d%7==0){
+//     console.log(d/7)
+// }else{
+//     console.log("bu eded 7-e bolunmur")
+// }
+// Input ilə daxil edilən bir string dəyərin uzunluğunun 5-dən böyük olub olmadığını yoxlayan alqoritm yazın.
+// let soz = prompt("sozu yaz")
+// if(String(soz).length>5){
+//     console.log("bu soz 5 herfden coxdur")
+// }else if(String(soz).length>=5){
+//     console.log("soz 5 herflidir")
+// }
+// else{
+//     console.log("bu soz 5 herfden azdir")
+// }
+// Input ilə daxil edilən iki boolean dəyərin hər ikisinin də true olub olmadığını yoxlayan alqoritm yazın.  sorusulasi sual
+// let a= prompt("birinci booleani daxil et")
+// let b= prompt("ikinci booleani daxil et")
+// if(a=='true' && b=='true'){
+//     console.log("her iki deyer true-dur")
+// }else{
+//     console.log("her iki deyer true deyildir")
+// }
+// Input ilə daxil edilən temperaturun (Selsi ilə) 0 dərəcədən aşağı olub olmadığını yoxlayan alqoritm yazın (donma nöqtəsi).
+// let a = + prompt("temperaturu daxil et")
+// if (a<=0){
+//     console.log("Havada saxta  var "+a)
+// }else{
+//     console.log("Havasa istilik " +a)
+// }
+// Input ilə daxil edilən bir rəqəmin (0-9 arasında) tək rəqəm olub olmadığını yoxlayan alqoritm yazın.
+// let a= +prompt("1-9 arasi reqem daxil et")
+// if(a>0 && a<=9 && a%2==0){
+//     console.log("daxil etdiyiniz eded cutdur")
+// }else{
+//     console.log("eded tekdir")
+// }
+// Input ilə daxil edilən bir ilin uzun il (leap year) olub olmadığını yoxlayan alqoritm yazın (4-ə bölünürsə və 100-ə bölünmürsə, və ya 400-ə bölünürsə).
+// let year=+prompt("ili daxil et")
+// if(year%400==0){
+//     console.log("bu il uzun ildir")
+// }else{
+//     console.log("bu il uzun il deyildir")
+// }
+// Input ilə daxil edilən bir string dəyərin boş olub olmadığını yoxlayan alqoritm yazın.
+// let soz= prompt("deyer daxil et")
+// if (soz===null || soz===undefined || soz.trim().length===0){
+//     console.log("Soz daxil edilmeyib")
+// }else{
+//     console.log("Soz yazilidi")
+// }
+// Input ilə daxil edilən bir ədədin kvadratının 100-dən böyük olub olmadığını yoxlayan alqoritm yazın.
+// let r=+prompt("reqem daxil et");
+// let cem=r*r;
+// if(cem>100){
+//     console.log(cem + " 100-den boyukdur")
+// }else {
+//     console.log(cem + " 100-den kicikdir")
+// }
+// Input ilə daxil edilən iki ədədin hasilinin müsbət, mənfi yoxsa sıfır olduğunu tapan alqoritm yazın.
+// let a=+prompt("birinci reqemi daxil edin");
+// let b= +prompt("ikinci reqemi daxil edin");
+// let cem=a*b;
+// if (cem<0){
+//     console.log( cem + " hasil menfidir")
+// }else if(cem==0){
+//     console.log(cem + " hasil 0-a beraberdir")
+// }else{
+//     console.log(cem + " hasil musbetdir")
+// }
+// Input ilə daxil edilən bir string dəyəri integer tipinə çevirməyə çalışan və uğurlu olub olmadığını bildirən alqoritm yazın. (Nümunə: "123" çevrilə bilər, "abc" yox).
+// Input ilə daxil edilən istifadəçi yaşının 18-dən böyük və ya bərabər olub olmadığını yoxlayan alqoritm yazın.
+// let yas= +prompt("Yasinizi daxil edin");
+// if(yas>=18){
+//     console.log("yasiniz 18dir veya 18-den boyukdur")
+// }else{
+//     console.log("yasiniz 18-den kicikdir")
+// }
+// Input ilə daxil edilən üçbucaq tərəflərinin (a, b, c) üçbucaq bərabərsizliyini (a+b>c, a+c>b, b+c>a) ödəyib ödəmədiyini yoxlayan alqoritm yazın.
+// let a=+prompt("ilk terefin olcusu")
+// let b=+prompt("ikinci terefin olcusu")
+// let c=+prompt("ucuncu terefin olcusu")
+// if(a+b>c && a+c>b && b+c>a){
+//     console.log("bu tereflerle ucbucaq qurmaq olar")
+// }else{
+//     console.log("bu tereflerle  ucbucaq qurmaq mumkun deyil")
+// }
+// Input ilə daxil edilən bir ədədin həm 3-ə, həm də 5-ə tam bölünüb bölünmədiyini yoxlayan alqoritm yazın.
+// let a= +prompt("bir eded daxil edin")
+// if(a%3==0 && a%5==0){
+//     console.log("bu eded hem 3-e, hem de 5-e bolunur")
+// }else{
+//     console.log("eded ne 3-e nede 5-e bolunur")
+// }
+// Input ilə daxil edilən bir float ədədi ən yaxın tam ədədə yuvarlaqlaşdıran (conversion) və nəticəni çıxaran alqoritm yazın.
+// let a=+prompt("istenilen eded daxil et")
+// let yuvarlaqlaşdirma = Math.round(a);
+// {
+//     console.log("edede en yaxin yuvaqlaq " + yuvarlaqlaşdirma)
+// }
+// Input ilə daxil edilən bir balın (0-100 arası) "A" (90-100), "B" (80-89), "C" (70-79), "D" (60-69), "F" (<60) kateqoriyalarından hansına aid olduğunu müəyyən edən alqoritm yazın. (Yalnız bir şərt bloku istifadə edin).
+// let bal = +prompt("Balinizi daxil edin");
+// if (bal >= 0 && bal <= 100) {
+//   let qiymet = bal >= 91 ? "A" : bal >= 81 ? "B" : bal >= 71 ? "C" : bal >= 61 ? "D" : bal >=51 ? "E" : "Kesildiniz"
+//   console.log("baliniz: " + qiymet);
+// } else {
+//   console.log("baliniz 0-100 arasinda olmalidir");
+// }
+// Input ilə daxil edilən iki string dəyərin eyni olub olmadığını, böyük-kiçik hərf fərqinə məhəl qoymadan (case-insensitive comparison) yoxlayan alqoritm yazın.
+// Input ilə daxil edilən bir ədədin kvadrat kökünün tam ədəd olub olmadığını yoxlayan alqoritm yazın. (Məsələn, 25 üçün kök 5-dir (tam), 26 üçün deyil).
+var a = +prompt("kvadrat koku daxil et");
+var kvadrat = Math.sqrt(a);
+if (kvadrat % 1 === 0) {
+    console.log("ededin koku: " + kvadrat);
+}
+else {
+    console.log("ededin koku tam deyil");
+}
+// Input ilə daxil edilən bir boolean dəyərin tərsini (inversiyasını) ekrana çıxaran alqoritm yazın (true üçün false, false üçün true).
+// Input ilə daxil edilən bir saatın (0-23 arası) günün hansı hissəsinə aid olduğunu (səhər, günorta, axşam, gecə) təyin edən alqoritm yazın (məsələn, 6-11 səhər, 12-17 günorta və s.).
+// let saat= +prompt("hal hazirda saat necedir")
+// if (saat>=0 && saat <= 6 ){
+//     console.log("saat " + saat + " gecedir")
+// }else if(saat>6 && saat<=12){
+//     console.log("saat" + saat + " seherdir")
+// }else if(saat>12 && saat<=18){
+//     console.log("saat" + saat + " gunortadir")
+// }else if(saat>18 && saat<=24){
+//     console.log("saat" + saat + " axsamdir")
+// }else{
+//     console.log("saati duzgun daxil et")
+// }
+// Input ilə daxil edilən iki ədəddən birincisinin ikincisindən böyük, kiçik və ya ona bərabər olduğunu müəyyən edib ekrana çıxaran alqoritm yazın
+// let a=  +prompt("ilk ededi yaz")
+// let b= +prompt("ikinci ededi yaz")
+// if(a==b){
+//     console.log(a + " ve" + b + " ededleri beraberdir")
+// }else if (a>b){
+//     console.log( a + " " + b +"-den boyukdur")
+// }else {
+//     console.log(b + " " + a + "-dan boyukdur")
+// }
